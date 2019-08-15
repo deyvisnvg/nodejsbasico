03b18e4a551551bdb65e0ab9f88a3fc94465c24e
Neyser <deyvisnvg@gmail.com>
2019-08-15 10:54:20 -0500

    Manejando modulos externo con NPM y require()

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
  * *************
  * 
  */
+
+ const cowsay = require("cowsay"); //Para poder referir siempre a un modulo que se  encuentra dentro de la carpeta node_modules y se trate como una 'dependencia externa' vamos a hacer un require sin ningun tipo de camino o ruta relativa (require("./"))
+
+ console.log(cowsay.say({
+     text: "Hellow, soy cowsay",
+     e: "oO",
+     U: "U"
+ }));

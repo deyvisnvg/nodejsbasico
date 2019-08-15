c9e2990ce079a52518503e0220ef67578f03a5e8
Neyser <deyvisnvg@gmail.com>
2019-08-15 09:01:01 -0500

    Modulos en Node - require() y module.exports

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
  * Comience aqui
  * *************
  */
+
+const math = require("./math");
+const greet = require("./greetings"); //Hace referencia directamente al index.js
+const hellow = require("./greetings/hello");
+
+console.log(math.add(4, 5)); // llamamos a la function add(){} con 'math.add'
+console.log(math.divide(4, 5));
+console.log(math.multiply(4, 5));
+console.log(math.substract(4, 5));
+
+console.log(greet.greet("Emir"));
+console.log(hellow.sayHello("Deyvis"));

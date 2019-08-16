fa5b9f8de14ce42ba4e452798c1ad98ebdd555e3
Neyser <deyvisnvg@gmail.com>
2019-08-16 18:58:17 -0500

    Ejecutando Callback y Promesas

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
  * *************
  * 
  */
+
+const call = require("./src/call");
+
+//  console.log(call.sync("Emir Salazar!"));
+// call.withCallback("Emir Salazar", call.sync); //call.sync (Sin parentesis)= Esto es un callback es como si generara la firma o lista por asi decirlo para ser ejecutada.
+call.withPromise("Emir Salazar")
+.then(dato => console.log(call.sync(dato)));

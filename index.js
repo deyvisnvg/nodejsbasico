a3634576557dd740fe1782feda229db3c837f83c
Neyser <deyvisnvg@gmail.com>
2019-08-17 15:08:35 -0500

    Ejemplo al ejecutar Promesas y async/await

--- index.js
+++ index.js
@@ -5,3 +5,32 @@
  * *************
  * 
  */
+const call = require("./src/call");
+
+// call.greet("Emir Salazar");
+
+// call.withPromise("Emir", "Salazar")
+// .then(dato => call.greet(dato))
+
+// call.withPromise("Emir", "Salazar")
+// .then(dato => {
+//     call.greet(dato);
+//     return call.promesa2("Deyvis", "Valdez")
+// })
+// .then(dato => {
+//     call.greet(dato);
+//     return call.promesa3("Marivi", "Valdez")
+// })
+// .then(dato => call.greet(dato))
+
+async function callWithPromise() {
+    const fullName = await call.withPromise("Emir", "Salazar") //async/await detiene la ejecución del programa hasta que todas las promesas sean resueltas, ya resueltas te resolverá todas las promesas en un solo resultado y no de una en una.
+    const fullName2 = await call.promesa2("Deyvis", "Salazar")
+    const fullName3 = await call.promesa3("Marivi", "Salazar")
+    // call.greet(fullName)
+    console.log(fullName);
+    console.log(fullName2);
+    console.log(fullName3);
+}
+
+callWithPromise();
--- src/call.js
+++ src/call.js
@@ -7,9 +7,27 @@ module.exports = {
         return new Promise((resolve, reject)=>{
             console.log("Ejecutando espera...");
             setTimeout(()=>{
-                console.log("Resolviendo la espera!");
-                resolve(`${nombre} ${apellido}`);
-            }, 5000); 
+                console.log("Resolviendo la espera!1");
+                resolve(`${nombre} ${apellido} 1`);
+            }, 3000);
+        })
+    },
+    promesa2: function(nombre, apellido) {
+        return new Promise((resolve, reject)=>{
+            console.log("Ejecutando espera...2");
+            setTimeout(()=>{
+                console.log("Resolviendo la espera!2");
+                resolve(`${nombre} ${apellido} 2`);
+            }, 3000);
+        })
+    },
+    promesa3: function(nombre, apellido) {
+        return new Promise((resolve, reject)=>{
+            console.log("Ejecutando espera...3");
+            setTimeout(()=>{
+                console.log("Resolviendo la espera!3");
+                resolve(`${nombre} ${apellido} 3`);
+            }, 3000);
         })
     }
 }

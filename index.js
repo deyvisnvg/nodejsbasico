6856f9729dda1b39991a48b3b3b6b03b371f7e3c
Neyser <deyvisnvg@gmail.com>
2019-08-19 15:27:03 -0500

    ejemplo de lectura y escritura por parte del usuario con readline

--- index.js
+++ index.js
@@ -27,17 +27,25 @@ const q = [
 
 const askQuestion = (rl, question) => {
     return new Promise((resolve, reject) => {
-        rl.question(question, answer => {
-            resolve(answer)
-        })
+        rl.question(question, answer => resolve(answer)) // Por cada pregunta recibida(segun la posición[i]) esperará una respuesta por parte del usuario. 
     })
 }
 
 const ask = question => {
     return new Promise(async resolve => {
-        
+        let resultados = [] // nuevo array vacío
+        for (let i = 0; i < question.length; i++) { // iteramos la posición del array(preguntas) menores a su longitud, donde i = 0, 1, 2
+            const result = await askQuestion(rl, question[i]); // Se le envía la pregunta segun la posición del ciclo[i]
+            resultados.push(result) // A la respuesta del usuario lo empujamos y lo guardamos en resultados[]
+        }
+        rl.close();
+        resolve(resultados);
     })
 }
+
+ask(q)
+.then(result => console.log(`Hola ${result[0]} ${result[1]}, tu edad es la siguiente: ${result[2]}`))
+
 
 // rl.question("Cuál es tu nombre?", answer => {
 //     console.log(`Hola, tu nombre es ${answer}`)

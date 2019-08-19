59271dc9aec26eec950feb81c02cf21d1f23abb0
Neyser <deyvisnvg@gmail.com>
2019-08-19 16:42:50 -0500

    utilizando el modulo child_process para ejecutar un comando desde node.js

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
  * *************
  *  
  */
+
+const cp = require("child_process"); // requerimos el módulo child_process
+
+function execCommand(commad) { // recibe el comando
+    cp.exec(commad, (err, stdout, stderr) => { // Este método envía un 'command' la cual genera un shell y luego ejecuta command dentro de ese shell. También envía un callback que proporciona una función y se llama con los argumentos 'error', 'stdout', 'stderr'
+    // En el exito, error será null. En caso de error, error será una instancia de error.
+    // Los argumentos stdout, stderr pasados a la devolución de llamada contendrán la salida stdout y stderr del proceso hijo, la salida o resultado con respecto al comando recibido.
+        if(err) {
+           console.log(`Error: ${err}`);
+           return;
+        }
+        if(stdout) { console.log(`Standar out: ${stdout}`) }
+        if(stderr) { console.log(`Standar err: ${stderr}`) }
+    })
+}
+
+execCommand("node ./src/script.js --base=5")

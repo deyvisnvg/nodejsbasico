0c226d62c89f7f9d1c7b711f59ed7bc10b36c835
Neyser <deyvisnvg@gmail.com>
2019-08-20 15:00:37 -0500

    Ejemplo de Manejo de errores en Node.js

--- index.js
+++ index.js
@@ -5,3 +5,36 @@
  * *************
  * 
  */
+
+// Ocurre que cuando se llama a una operación asíncrona 'fs.readFile()' dentro de un try{} catch{}, el catch sale 
+// del contexto de ejecución de la misma, cuando esta resuelve u obtiene su respuesta asíncrona; de tal forma que
+// si nosotros intentamos hacer un throw dentro de un try{} catch{} y de una función u operación que es asíncrona 
+// entonces este throw(error) no va ser capturado por el catch{} y realmente va a detener la ejecución del código.
+
+// Ojo: no lanzar throw dentro de funciones asíncronas y bloques con try{} catch{} y que son malas prácticas.
+
+const error = require("./src/errors");
+const handling = require("./src/handling");
+//===========================================================/
+// try {
+//     error.standardErr.range()
+// } catch (error) {
+//     console.log(error)
+// }
+
+// console.log("Hola");
+//===========================================================/
+
+//===========================================================/
+// try {
+//     handling.sync();
+// } catch (error) {
+//     console.log(error);
+// }
+
+// console.log("Hola papi");
+//===========================================================/
+
+//===========================================================/
+
+handling.errorFirstCallbackWrong();
--- src/handling.js
+++ src/handling.js
@@ -9,6 +9,14 @@ module.exports = {
             throw `This is a sync ${err}`;
         }
     },
+
+// Ocurre que cuando se llama a una operación asíncrona 'fs.readFile()' dentro de un try{} catch{}, el catch sale 
+// del contexto de ejecución de la misma, cuando esta resuelve u obtiene su respuesta asíncrona; de tal forma que
+// si nosotros intentamos hacer un throw dentro de un try{} catch{} y de una función u operación que es asíncrona 
+// entonces este throw(error) no va ser capturado por el catch{} y realmente va a detener la ejecución del código.
+
+// Ojo: no lanzar throw dentro de funciones asíncronas y bloques con try{} catch{} y que son malas prácticas.
+
     errorFirstCallbackWrong: function() {
         try {
             fs.readFile('/some/file/that/does-not-exist', (err, data) => {
@@ -21,6 +29,7 @@ module.exports = {
             // This will not catch the throw!
             console.error(`Catched err: ${err}`);
         }
+        console.log("Hola!");
     },
     errorFirstCallback: function() {
         fs.readFile('/some/file/that/does-not-exist', (err, data) => {

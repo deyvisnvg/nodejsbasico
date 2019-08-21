3d60893c64858bfddc95218206c71afed3a36bd9
Neyser <deyvisnvg@gmail.com>
2019-08-21 10:44:25 -0500

    Aplicando ejemplos con el módulo event

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
  * *************
  * 
  */
+const greet = require('./src/greet')
+
+greet.emit('clap');
+
+greet.emit('shout','Viva el Amor');
+
+greet.emit('call', 'Emir Salazar', nameRecibido => console.log(`Estamos llamando a: ${nameRecibido}`));
--- src/greet.js
+++ src/greet.js
@@ -1,6 +1,9 @@
-const EventEmitter = require('events');
-class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
+const EventEmitter = require('events'); // Requerimos al módulo 'events'
+class MyEmitter extends EventEmitter {} // Se extiende una Clase en base al emisorDeEventos(EventEmitter{})
+const myEmitter = new MyEmitter(); // Aquí se llama al constructor de la Clase para crear el emisor de eventos.
+
+// myEmitter.on('event', callback) = Le dice al emisorDeEventos: En la situación 'event' ejecuta la función que pasa como callback
+// myEmitter.emit('event') = Le dice emite la situación es decir el nombre de la situación que queremos que ocurra.
 
 myEmitter.on('clap', () => {
     console.log('A clapping event was produced.');

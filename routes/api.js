1bd4c323140837755951e1815a4b3fa1c270044a
Neyser <deyvisnvg@gmail.com>
2019-08-22 10:45:14 -0500

    Ejemplo de Contruccion de un API con Express.js

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,8 @@ module.exports = function(app){
         const query = db.getUsers();
         res.json(query);
     }),
-    app.get(`${API_BASE}/user/:id`, (req, res)=>{
-        const query = db.getUserById(req.params.id);
+    app.get(`${API_BASE}/user/:id`, (req, res)=>{ //Enviamos la id como parámetro en la URL del resultado que queremos obtener
+        const query = db.getUserById(req.params.id); //Utilizando 'req.params.id' va ha optener el 'id' que le pasamos como parámetro en la URL
         res.json(query);
     })
 };
--- routes/views.js
+++ routes/views.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     app.get("/route", (req, res)=>{
         res.send("I'm on the '/route' page.");
     });
-    app.get("*", (req, res)=>{
+    app.get("*", (req, res)=>{ //Debe estar al final siempre //foult handler = menejador de fallas, ruta alternativa en caso el fichero no exista.
         res.send("Where am I?");
     });
 }
